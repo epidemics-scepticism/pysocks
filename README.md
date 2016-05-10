2bef927bc7ec1ff843383bde1ff5e45cf5f614b0
amnesia <amnesia@amneia.invalid>
2016-05-10 00:35:18 +0000

    meh fuck lazy devs...

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # pysocks
 simple socks5h implementation in python
+
+a simple usage example:
+```
+import socks
+import socket
+s = socket.socket()
+s.connect(('127.0.0.1',9050))
+lol = socks.socks('fbi.gov',80)
+lol.dial(s)
+# 127.0.0.1:9050 -> fbi.gov:80
+s.send('GET /j-edgar-hoover-dick-pics.jpg HTTP/1.1\r\nHost: fbi.gov\r\n\r\n')
+...
+```
+a more complex example, chaining socks proxies together:
+```
+import socks
+import socket
+s = socket.socket()
+s.connect(('127.0.0.1', 9050))
+lol = socks.socks('1.2.3.4', 1080)
+lol.dial(s)
+# 127.0.0.1:9050 -> 1.2.3.4:1080
+lol.set_host('4.3.2.1')
+lol.set_port(8010)
+lol.dial(s)
+# 127.0.0.1:9050 -> 1.2.3.4:1080 -> 4.3.2.1:8010
+lol.set_host('fbi.gov')
+lol.set_port(80)
+lol.dial(s)
+# 127.0.0.1:9050 -> 1.2.3.4:1080 -> 4.3.2.1:8010 -> fbi.gov:80
+s.send('GET /j-edgar-hoover-dick-pics.jpg HTTP/1.1\r\nHost: fbi.gov\r\n\r\n')
+...
+```
--- socks.py
+++ socks.py
@@ -15,34 +15,11 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import socket,struct
-
-def send_all(s, payload):
-    """Ugly hack to send all"""
-    sent = 0
-    while sent < len(payload):
-        tmp = s.send(payload[sent:])
-        if tmp > 0:
-            sent += tmp
-
-def recv_all(s, buflen):
-    """Ugly hack to recv all"""
-    tmp = ''
-    while len(tmp) < buflen:
-        tmp += s.recv(buflen - len(tmp))
-    return tmp
-
-# >>> import socks
-# >>> lol = socks.socks('fbi.gov',80)
-# >>> s = lol.lazy_dev('127.0.0.1',9050)
-# >>> s.send('GET / HTTP/1.1\r\nHost: fbi.gov.\r\n\r\n')
-# >>> s.recv(4096)
-# 'HTTP/1.0 301 Moved Permanently\r\nLocation: https://www.fbi.gov/\r\nContent-Length: 224\r\n\r\n<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd"><html><head><title>Web Forwarding</title><meta http-equiv="Content-Type" content="text/html;charset=utf-8"></head><body></body></html>'
+import struct
 
 class socks():
     """Provides a simple SOCKS5H implementation"""
     version = 5
-    methods = [2, 0]
     failcodes = {
             0: 'success',
             1: 'general SOCKS server failure',
@@ -54,41 +31,73 @@ class socks():
             7: 'command not supported',
             8: 'address type not supported',
             }
-    def __init__(self, h='fbi.gov', p=80, user='', passwd=''):
+    def __init__(self, h='', p=0, user=None, passwd=None):
+        """setup the initiate state"""
         self.set_host(h)
         self.set_port(p)
-        self.set_user(user)
-        self.set_pass(passwd)
+        if user != None and passwd != None:
+            self.methods = [0,2]
+            self.set_user(user)
+            self.set_pass(passwd)
+        else:
+            self.methods = [0,]
     def set_host(self, h=None):
+        """set the host we will ask the socks server to connect to"""
         if h != None:
             h = str(h)
             if len(h) > 255:
                 raise Exception('Hostname must be less than 256 bytes: got %d bytes' % len(h))
             self.h = h
     def set_port(self, p=None):
+        """set the port we will ask the socks server to connect to"""
         if p != None:
             self.p = int(p)
     def set_user(self, s=None):
+        """set the username to authenticate with, enable socksauth"""
         if s != None:
             user = str(s)
             if len(user) > 255:
                 raise Exception('Username must be less than 256 bytes: got %d bytes' % len(user))
             self.user = user
+            self.set_auth(True)
     def set_pass(self, s=None):
+        """set the password to authenticate with, enable socksauth"""
         if s != None:
             passwd = str(s)
             if len(passwd) > 255:
                 raise Exception('Password must be less than 256 bytes: got %d bytes' % len(passwd))
             self.passwd = passwd
+            self.set_auth(True)
+    def set_auth(self, b=False):
+        """toggle if we will avertise user/pass auth"""
+        if b:
+            self.methods = [2,0]
+        else:
+            self.methods = [0,]
+    def send_all(self, s, payload):
+        """ugly hack to send all"""
+        sent = 0
+        while sent < len(payload):
+            tmp = s.send(payload[sent:])
+            if tmp > 0:
+                sent += tmp
+    def recv_all(self, s, buflen):
+        """ugly hack to recv all"""
+        tmp = ''
+        while len(tmp) < buflen:
+            tmp += s.recv(buflen - len(tmp))
+        return tmp
     def dial(self, s):
+        """use open socket s to negotiate a socks connection"""
         self.negotiate_auth(s)
         self.negotiate_connection(s)
     def negotiate_auth(self, s):
+        """negotiate an authentication method"""
         payload = struct.pack('!BB', self.version, len(self.methods))
         for method in self.methods:
             payload += struct.pack('!B', method)
-        send_all(s, payload)
-        v, m = struct.unpack('!BB', recv_all(s, 2))
+        self.send_all(s, payload)
+        v, m = struct.unpack('!BB', self.recv_all(s, 2))
         if v != 5:
             raise Exception('Invalid SOCKS version: %d' % v)
         if not m in self.methods:
@@ -96,40 +105,38 @@ class socks():
         if m is 2:
             self.userpassauth(s)
     def userpassauth(self, s):
+        """handle username and password authentication"""
         payload  = struct.pack('!BB', 1, len(self.user))
         payload += self.user
         payload += struct.pack('!B', len(self.passwd))
         payload += self.passwd
-        send_all(s, payload)
-        v, s = struct.unpack('!BB', recv_all(s, 2))
+        self.send_all(s, payload)
+        v, s = struct.unpack('!BB', self.recv_all(s, 2))
         if v != 1:
             raise Exception('Invalid SOCKS User/Pass version: %d' % v)
         if s != 0:
             raise Exception('Invalid SOCKS User/Pass credentials: %d' % status)
     def conn_fail(self, r):
+        """report why the connection failed by status code"""
         return self.failcodes.get(r, "unknown")
     def negotiate_connection(self, s):
+        """negotiate the socks connection to our chosen target"""
         payload  = struct.pack("!BBBB", self.version, 1, 0, 3)
         payload += struct.pack("!B", len(self.h))
         payload += self.h
         payload += struct.pack('!H', int(self.p))
-        send_all(s, payload)
-        v, r, _, t = struct.unpack('!BBBB', recv_all(s, 4))
+        self.send_all(s, payload)
+        v, r, _, t = struct.unpack('!BBBB', self.recv_all(s, 4))
         if v != 5:
             raise Exception('Invalid SOCKS version: %d' % v)
         if r != 0:
             raise Exception('Failed to connect: %s' % self.conn_fail(r))
         if t == 1: # ipv4 + port
-            _ = recv_all(s, 6)
+            _ = self.recv_all(s, 6)
         elif t == 4: # ipv6 + port
-            _ = recv_all(s, 18)
+            _ = self.recv_all(s, 18)
         elif t == 3: # domain name + port
-            l, = struct.unpack('!B', recv_all(s, 1))
-            _ = recv_all(s, l + 2)
+            l, = struct.unpack('!B', self.recv_all(s, 1))
+            _ = self.recv_all(s, l + 2)
         else:
             raise Exception('Invalid address type: %d' % t)
-    def lazy_dev(self, sockshost='127.0.0.1', socksport=9050):
-        s = socket.socket()
-        s.connect((str(sockshost),int(socksport)))
-        self.dial(s)
-        return s
